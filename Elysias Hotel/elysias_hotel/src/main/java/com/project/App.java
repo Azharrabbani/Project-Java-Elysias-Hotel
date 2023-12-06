6f819a88b83a07931c1e62066730b145c8f1d6d2
Azharrabbani <151945957+Azharrabbani@users.noreply.github.com>
2023-12-07 00:01:16 +0700

    Update App.java

--- Elysias Hotel/elysias_hotel/src/main/java/com/project/App.java
+++ Elysias Hotel/elysias_hotel/src/main/java/com/project/App.java
@@ -4,16 +4,27 @@ import com.project.Room.Room;
 import com.project.User.Guest;
 import com.project.User.User;
 
+import java.util.Scanner;
+
 
 public class App
 {
     public static void main( String[] args )
     {
         // java fx buat interaktif
-        User user = new User("azhar", "password"); // Awal user masukin username dan password buat jadi interaktif
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("UserName : " );     // Gambaran untuk input dari pengguna sesuaikan dengan java fx
+//        String userName = input.nextLine();
+//        System.out.print("Password : " );
+//        String password = input.nextLine();
+
+        User user = new User("kita", "password"); // Awal user masukin username dan password buat jadi interaktif
         Guest guest = new Guest(user);
+
+
+
         //User Login dengan method login
-//        if (guest.Login(user1)){
+//        if (guest.Login(user)){
 //            System.out.println("Berhasil Login"); // kalau berhasil user diarahkan ke halaman utama
 //        }
 //        else{
@@ -29,47 +40,54 @@ public class App
 //            guest.AddProfile(user); // User diarahkan ke halaman add profile untuk mengisi profile mereka
 //        }
 
-//        guest.DelAccount(user); // untuk delete akun user
 
+//        guest.DelAccount(user); // untuk delete akun user
+//
 //        guest.UpdateProfile(user); // Update Profile user diarahkan ke halaman untuk update profile
 
 
-//        guest.DisplayProfile(guest);
+//        guest.DisplayProfile(user);
 
-//        guest.DisplayProfile(guest);
 //        guest.getNama(guest); // Mendapatkan nama user
 //        guest.getNoTel(guest); // Mendapatkan noTel user
 //        guest.getAlamat(guest); // Mendapatkan Alamat user
 //        guest.getKota(guest);  // Mendapatkan kota user
 
         // Wallet
-//        guest.addWallet(guest); // method untuk membuat walelt
 //        guest.getSaldo(guest); // method untuk mengambil balance
 //
         // java fx buat interaktif
-//        guest.UpdateSaldo(guest, 200000); // method untuk menambah balance wallet (buat interaktif)
-//        guest.checkcSaldo(guest);
+//        guest.UpdateSaldo(guest, 300000); // method untuk menambah balance wallet (buat interaktif)
+//        guest.checkSaldo(guest);
 
-        
-        // Room abaikan dulu
+
+
         // Room
-        // Room room = new Room(204);
-        // room.bookRoom(guest,room);
+//        Room room = new Room(105); // Masukkan Room Number bisa pake Scanner atau Input
+//
+//        // Book Room
+//        if (guest.bookRoom(guest,room)){
+//            System.out.println("Berhasil Booking");
+//        }
+//
+//        // Display Room Sesuai Room Number Yang di Masukkan
 //        room.displayRoom(room);
-//        room.UpdateRoomBooked(room);
-//        room.UpdateRoomAvailable(room);
-//        room.displayRoom(room);
-//        guest.UpdateSaldoPayment(guest, room);
-//        guest.checkcSaldo(guest);
-
-
-
-
-
-
-
-
-
+//
+//
+//        // Menampilkan History Reservation Guest
+//        guest.DisplayReservation(guest);
+//
+//        // Cancel Booking
+//        guest.cancelBooking(guest, room);
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
     }
