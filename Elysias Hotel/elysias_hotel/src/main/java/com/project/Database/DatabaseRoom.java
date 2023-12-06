7edc174a08f9ce13b915e4cd3da93dbc4e61e08c
Azharrabbani <151945957+Azharrabbani@users.noreply.github.com>
2023-12-06 23:52:41 +0700

    update dan menambah beberapa method

--- Elysias Hotel/elysias_hotel/src/main/java/com/project/Database/DatabaseRoom.java
+++ Elysias Hotel/elysias_hotel/src/main/java/com/project/Database/DatabaseRoom.java
@@ -5,9 +5,13 @@ import com.project.Room.RoomList;
 import com.project.User.Guest;
 import util.ConnectionUtil;
 
+import java.net.ConnectException;
 import java.sql.*;
 
 public abstract class DatabaseRoom implements RoomList {
+
+
+    // Mengecek Room (Untuk digunakan di method lain)
     @Override
     public boolean checkIfAvailable(Room room){
         try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
@@ -24,6 +28,8 @@ public abstract class DatabaseRoom implements RoomList {
 
     }
 
+
+    // Mengubah Status Room yang awalnya AVAILABLE menjadi BOOKED
     public void UpdateRoomBooked(Room room){
         try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
             String sql = "UPDATE ROOM SET Status = 'BOOKED' WHERE Room_Number = ?";
@@ -36,6 +42,8 @@ public abstract class DatabaseRoom implements RoomList {
         }
     }
 
+
+    // Method untuk mengupdate room Menjadi AVAILABLE (digunakan di method lain dan Method yang digunakan Oleh ADMIN)
     public void UpdateRoomAvailable(Room room){
         try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
             String sql = "UPDATE ROOM SET Status = 'AVAILABLE' WHERE Room_Number = ?";
@@ -48,6 +56,22 @@ public abstract class DatabaseRoom implements RoomList {
         }
     }
 
+
+    // Update status history guest
+    public void UpdateReserveStatus(Guest guest, Room room){
+        try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
+            String sql = "UPDATE Reservation SET Status = 'COMPLETE' WHERE Username = ? AND Room_Number = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)){
+                statement.setString(1, guest.getUserName());
+                statement.setInt(2, room.getRoom_Number());
+                statement.executeUpdate();
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Mengambil Price dari masing masing room
     public double getPrice(Room room){
         try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
             String sql = "SELECT * FROM Room WHERE Room_Number = ?";
@@ -66,20 +90,61 @@ public abstract class DatabaseRoom implements RoomList {
         return 0;
     }
 
-    @Override
-    public void bookRoom(Guest guest, Room room) {
+
+    public double getNight(Guest guest){
         try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
-            String sql = "INSERT INTO Reservation(NoTel, Room_Number, CheckIn, CheckOut, Night) VALUES(?, ?, ?, ?, ?)";
+            String sql = "SELECT * FROM Reservation WHERE Room_Number = ?";
             try (PreparedStatement statement = connection.prepareStatement(sql)){
-                statement.setString(1, guest.getNoTelGuest(guest));
+                statement.setString(1, guest.getUserName());
+                try (ResultSet resultSet = statement.executeQuery()){
+                    if (resultSet.next()){
+                        double night = resultSet.getInt("Night");
+                        return night;
+                    }
+                }
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Number Not Found !!");
+        }
+        return 0;
+    }
+
+
+    // Book Room
+    public boolean bookRoom(Guest guest, Room room) {
+        try (Connection connection = ConnectionUtil.getDataSource().getConnection()) {
+            String sql = "INSERT INTO Reservation(UserName, Room_Number, CheckIn, CheckOut, Night, Total) VALUES(?, ?, ?, ?, ?, ?)";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, guest.getUserName());
                 statement.setInt(2, room.getRoom_Number());
                 statement.setDate(3, room.getCheckIn());
                 statement.setDate(4, room.getCheckOut());
                 statement.setLong(5, room.getNight());
+                statement.setDouble(6, room.getTotal());
 
-                if (checkIfAvailable(room)){
+                if (checkIfAvailable(room)) {
                     int rowsEffected = statement.executeUpdate();
-                    System.out.println(rowsEffected > 0 ? "Booking Successful" : "Booking Failed");
+                    return rowsEffected > 0; // Return true jika booking berhasil
+                } else {
+                    System.out.println("Booking Failed: Room Already BOOKED");
+                    return false; // Return false jika booking gagal karena kamar sudah dipesan
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    // Pemisalan untuk history yang belum complete
+    public boolean isNotComplete(Guest guest, Room room){
+        try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
+            String sql = "SELECT * FROM Reservation WHERE UserName = ? AND Room_Number = ? AND Status = 'NOT COMPLETE'";
+            try (PreparedStatement statement = connection.prepareStatement(sql)){
+                statement.setString(1, guest.getUserName());
+                statement.setInt(2, room.getRoom_Number());
+                try (ResultSet resultSet = statement.executeQuery()){
+                    return resultSet.next();
                 }
             }
         }catch (SQLException e){
@@ -87,11 +152,24 @@ public abstract class DatabaseRoom implements RoomList {
         }
     }
 
-    @Override
-    public void cancelBooking() {
 
+    // Cancel Booking
+    @Override
+    public void cancelBooking(Guest guest, Room room) {
+        try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
+            String sql = "DELETE FROM Reservation WHERE Room_Number = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)){
+                statement.setInt(1, room.getRoom_Number());
+                int rowsEffected = statement.executeUpdate();
+                System.out.println(rowsEffected > 0 ? "Success Cancel" : "Failed To Cancel");
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
+
+    // Menampilkan Room
     @Override
     public void displayRoom(Room room) {
         try (Connection connection = ConnectionUtil.getDataSource().getConnection()){
@@ -113,4 +191,29 @@ public abstract class DatabaseRoom implements RoomList {
             throw new RuntimeException(e);
         }
     }
+
+
+    // Method ini digunakan di method lain
+    public double getPriceFromReservation(Guest guest, Room room) {
+        try (Connection connection = ConnectionUtil.getDataSource().getConnection()) {
+            String sql = "SELECT * FROM Reservation WHERE Room_Number = ?";
+
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, room.getRoom_Number());
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    if (resultSet.next()) {
+                        double price = resultSet.getDouble("total");
+                        return price;
+                    } else {
+                        System.out.println("Reservasi tidak ditemukan untuk nomor kamar: " + room);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
+    }
+
+
 }
