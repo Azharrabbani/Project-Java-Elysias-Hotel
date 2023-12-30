57afb90ecf1f9f03703af5360959ddb172af8705
Azharrabbani <151945957+Azharrabbani@users.noreply.github.com>
2023-12-31 03:48:49 +0700

    Delete Elysias Hotel/elysias_hotel directory

--- Elysias Hotel/elysias_hotel/src/main/java/com/project/Room/Type.java
+++
@@ -1,8 +0,0 @@
-// Abaikan
-
-package com.project.Room;
-
-public enum Type {
-    DELUXE,
-    PREMIUM
-}
--- Elysias Hotel/elysias_hotel/src/main/java/com/project/User/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.project.User;
-
-public interface UserService {
-    public boolean Login(User user);
-    public boolean SignUp(User user);
-    public void DelAccount(User user);
-    public void AddProfile(User user);
-
-}
